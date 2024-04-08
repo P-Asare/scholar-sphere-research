a89131a4ada0a351ed5f228e187d7bbf899270a9
P-Asare <pinopalal11@gmail.com>
2024-04-08 08:07:00 +0100

    include mini component routing

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -7,11 +7,21 @@ import { PostsContext } from '../contexts/PostsContext';
 function MiddleSection({activeSection}){
     const {posts} = useContext(PostsContext);
 
-    const postsList = posts.map((post) => <UserPost key={post.id} post={post}/>);
+    let filteredPosts = posts;
+    // TODO: Include Favorite context here
+    // if(activeSection === 'favorite'){
+    //     filteredPosts = posts.filter(post => ...);
+    // }
+
+    const postsList = filteredPosts.map((post) => <UserPost key={post.id} post={post}/>);
 
     return(
         <div className="main_container">
-            {postsList}
+            {
+            (activeSection === "home" && postsList) ||
+            (activeSection === "favorites" && <p>Favorite</p>) ||
+            (activeSection === "project" && <p>Project</p>)
+            }
         </div>
     );
 }
