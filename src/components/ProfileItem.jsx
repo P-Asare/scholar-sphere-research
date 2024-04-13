ab455dcaf68e5c8cfbc8402a2d327910b7cac3d5
P-Asare <pinopalal11@gmail.com>
2024-04-13 20:29:16 +0100

    add pop ups to verify acceptance and decline

--- src/components/ProfileItem.jsx
+++ src/components/ProfileItem.jsx
@@ -1,6 +1,8 @@
 import '../styles/ProfileItem.css';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
+import Swal from 'sweetalert2';
 
 /**
  * 
@@ -8,6 +10,44 @@ import CloseIcon from '@mui/icons-material/Close';
  * @returns 
  */
 function ProfileItem({follower, isright, isapplication}){
+
+    const [isAccepted, setIsAccepted] = useState();
+
+    const handlePickClick = async (id, action) => {
+        try {
+            const response = await fetch(`http://localhost:80/scholar-sphere/actions/accept_decline_request_action.php?action=${action}&pen_id=${id}`, {
+                method: 'GET',
+                headers: {
+                    'content-type':'application/json'
+                }
+            });
+
+            if(!response.ok){
+                throw new Error('Pick failed');
+            }
+
+            setIsAccepted(action);
+
+            const data = await response.json()
+
+            if (isAccepted === 1) {
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'You approved the student as a collaborator!',
+                    icon: 'success',
+                });
+            } else if(isAccepted === 2){
+                Swal.fire({
+                    title: 'Rejected!',
+                    text: 'You rejected the user!',
+                    icon: 'error',
+                });
+            }
+
+        } catch (error) {
+            console.error('Error following user:', error);
+        }
+    };
 
     return(
         <div className="profile-container">
@@ -25,8 +65,8 @@ function ProfileItem({follower, isright, isapplication}){
             </div>
             {isapplication ? (
                 <div className="actions">
-                    <CheckIcon sx={{color: "green", fontSize: 20}}/>
-                    <CloseIcon sx={{color: "red", fontSize: 20}} />
+                    <CheckIcon sx={{color: "green", fontSize: 20}} onClick={() => handlePickClick(follower.id, 1)}/>
+                    <CloseIcon sx={{color: "red", fontSize: 20}} onClick={() => handlePickClick(follower.id, 2)} />
                 </div>
             ) : (
                 <p></p>
